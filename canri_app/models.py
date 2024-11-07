28d1a90234223534b9188da5c1691134b733329d
mknhm1 <ktq2390308@stu.o-hara.ac.jp>
2024-11-07 12:41:55 +0900

    モデル少しできた

--- canri_app/models.py
+++ canri_app/models.py
@@ -12,3 +12,22 @@ class User(models.Model):
 
     class Meta:
         db_table = 'User'  # テーブル名を指定
+
+class Category(models.Model):
+    CategoryID = models.AutoField(primary_key=True)
+    Category_Name = models.CharField(max_length=50)  # VARCHAR(255)
+    Detail=models.CharField(max_length=10000)
+    creation_date = models.DateTimeField(auto_now_add=True)  # DATETIME
+    deletion_date = models.DateTimeField(null=True, blank=True)  # DATETIM
+    deletion_flag = models.BooleanField(default=False)  # BOOLEAN
+
+    class Meta:
+        db_table = 'Category'  # テーブル名を指定
+
+
+
+class Project(models.Model):
+    ProjectID=models.AutoField(primary_key=True)
+    Project_Name=models.CharField(max_length=50)
+    Project_Detail=models.CharField(max_length=10000)
+    Project_start_date=models.DateField()
