f0c7b953163c147fb8c2132257d9990a9635001d
myuaaryve <ktq2390322@stu.o-hara.ac.jp>
2024-11-07 11:10:38 +0900

    Please enter the commit message for your changes. Lines starting with '#' will be ignored, and an empty message aborts the commit.
    On branch back
    Your branch is up to date with 'origin/back'.
    
    Changes to be committed:
    	modified:   accounts/admin.py
    	modified:   accounts/forms.py
    	modified:   accounts/models.py
    	modified:   accounts/views.py
    	modified:   canri/settings.py
    	modified:   canri/urls.py

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
+from .models import CustomUser
 
 # Register your models here.
+
+class CustomUserAdmin(admin.ModelAdmin):
+
+    list_display = ('id', 'password', 'name')
+    list_display_links =  ('id', 'password', 'name')
+
+
+admin.site.register(CustomUser, CustomUserAdmin)
