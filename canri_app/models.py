49768b92adf1c248c7213da83a8c5ac5ca2c92d6
gayo3n <ktq2390310@stu.o-hara.ac.jp>
2024-11-07 12:22:10 +0900

    Please enter the commit message for your changes. Lines starting with '#' will be ignored, and an empty message aborts the commit.
    On branch back
    Your branch is up to date with 'origin/back'.
    
    Changes to be committed:
    	modified:   canri_app/models.py
    	modified:   canri_app/urls.py

--- canri_app/models.py
+++ canri_app/models.py
@@ -1,14 +1,5 @@
 from django.db import models
+from django.utils import timezone
+from django.contrib.auth.models import Member
 
-class User(models.Model):
-    name = models.CharField(max_length=100)  # VARCHAR(100)
-    password = models.CharField(max_length=255)  # VARCHAR(255)
-    creation_date = models.DateTimeField(auto_now_add=True)  # DATETIME
-    deletion_date = models.DateTimeField(null=True, blank=True)  # DATETIME
-    update_date = models.DateTimeField(auto_now=True)  # DATETIME
-    deletion_flag = models.BooleanField(default=False)  # BOOLEAN
-    administrator_flag = models.BooleanField(default=False)  # BOOLEAN
-    user_id = models.AutoField(primary_key=True)  # INTEGER PRIMARY KEY AUTOINCREMENT
-
-    class Meta:
-        db_table = 'User'  # テーブル名を指定
+# class Member (models.Model):
--- canri_app/urls.py
+++ canri_app/urls.py
@@ -4,4 +4,5 @@ from . import views
 app_name ='canri_app'
 urlpatterns = [
     path('', views.IndexView.as_view(), name='index'),
+    path('/ongoingproject', views.OngoinprojectView.as_view(), name='ongoingproject')
 ]
