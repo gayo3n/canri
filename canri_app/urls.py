c8ab975d0e856ccad6c5bfe59e68f27a827e450c
berry0113 <ktq2390307@stu.o-hara.ac.jp>
2024-11-08 14:12:08 +0900

    Please enter the commit message for your changes. Lines starting with '#' will be ignored, and an empty message aborts the commit.
    On branch front
    Your branch is behind 'origin/front' by 3 commits, and can be fast-forwarded.
      (use "git pull" to update your local branch)
    
    Changes to be committed:
    	modified:   canri_app/__pycache__/urls.cpython-311.pyc
    	modified:   canri_app/__pycache__/views.cpython-311.pyc
    	modified:   canri_app/urls.py
    	modified:   canri_app/views.py
    	modified:   templates/base2.html
    	modified:   templates/index.html
    	modified:   templates/login.html

--- canri_app/urls.py
+++ canri_app/urls.py
@@ -5,5 +5,7 @@ app_name ='canri_app'
 
 urlpatterns = [
     path('', views.IndexView.as_view(), name='index'),
+    path('menberlist/', views.IndexView.as_view(), name='menberlist'),
+    path('management_account/', views.IndexView.as_view(), name='management_account'),
     # path('ongoingproject/', views.OngoinprojectView.as_view(), name='ongoingproject') # 進行中プロジェクト
 ]
--- canri_app/views.py
+++ canri_app/views.py
@@ -4,3 +4,5 @@ from django.views.generic.base import TemplateView
 # Create your views here.
 class IndexView (TemplateView):
     template_name = "index.html"
+    template_name = "menberlist.html"
+    template_name = "management_account.html"
