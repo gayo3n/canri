594378c8d70602810269cae02171ddc26d760b84
gayo3n <ktq2390310@stu.o-hara.ac.jp>
2024-11-08 11:48:53 +0900

    index!!!!!

--- accounts/views.py
+++ accounts/views.py
@@ -7,7 +7,22 @@ from . forms import LoginForm
 from django.contrib.auth.forms import AuthenticationForm
 from django.contrib.auth.mixins import LoginRequiredMixin
 
+from django.shortcuts import render
+from django.views.generic.base import TemplateView
 
+
+
+class LoginView(TemplateView):
+    template_name = 'login.html'
+
+class LoginCompView(TemplateView):
+    template_name = 'login_complite.html'
+
+class LogoutView(TemplateView):
+    template_name = 'logout.html'
+
+class MwnuView(TemplateView):
+    template_name = 'menu.html'
 
 
 #ログイン機能
