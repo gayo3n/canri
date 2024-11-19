10babca525d819ecbf78315f80e0e3e05a5f0838
mknhm1 <ktq2390308@stu.o-hara.ac.jp>
2024-11-19 12:28:51 +0900

    account url修正

--- accounts/urls.py
+++ accounts/urls.py
@@ -3,7 +3,7 @@ from django.urls import path, include
 from django.contrib.auth.views import LoginView, LogoutView
 from . import views
 
-from.views import acclogin, logincomp, logout, account_create, account_create_complete,manage_account, account_delete, account_change_employee
+from.views import acclogin, logincomp, logout, account_create, account_create_complete,manage_account, account_delete, account_change_employee,ManagementAccountView
 
 app_name = 'accounts'
 
@@ -14,7 +14,9 @@ urlpatterns = [
     path('login_failure/', views.LoginFailView.as_view(), name='login_failure'), #ログイン失敗
     path('logout_confirmation/', views.logout, name='logout_confirmation'), #ログアウト
     path('logout_complete/', views.LogoutCompView.as_view(), name='logout_complete'), #ログアウト完了
-    path('management_account/', views.manage_account, name='manage_account'), #アカウント一覧
+    # path('management_account/', views.ManagementAccountView, name='manage_account'), #アカウント一覧
+
+    path('management_account/', ManagementAccountView.as_view(), name='manage_account'),
     path('management_account/account_creating/', views.account_create, name='account_creating'), #アカウント作成
     path('management_account/create/account_create_complete/', views.account_create_complete, name='account_create_complete'),  #アカウント作成完了
     path('account_delete/', views.account_delete, name='account_delete'), #アカウント削除
