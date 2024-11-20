b18b233ccb0cc87879f82c23c74596925bba0528
berry0113 <ktq2390307@stu.o-hara.ac.jp>
2024-11-20 14:20:39 +0900

    Please enter the commit message for your changes. Lines starting with '#' will be ignored, and an empty message aborts the commit.
    On branch front
    Your branch is behind 'origin/front' by 7 commits, and can be fast-forwarded.
      (use "git pull" to update your local branch)
    
    Changes to be committed:
    	modified:   accounts/migrations/__pycache__/0001_initial.cpython-311.pyc
    	modified:   accounts/migrations/__pycache__/0002_rename_administrator_flag_user_is_staff_and_more.cpython-311.pyc
    	modified:   accounts/migrations/__pycache__/0003_user_administrator_flag_user_deletion_flag.cpython-311.pyc
    	modified:   accounts/migrations/__pycache__/0004_alter_user_table.cpython-311.pyc
    	modified:   accounts/migrations/__pycache__/0005_user_is_active.cpython-311.pyc
    	modified:   accounts/migrations/__pycache__/__init__.cpython-311.pyc
    	modified:   canri_app/__pycache__/api.cpython-311.pyc
    	modified:   canri_app/__pycache__/urls.cpython-311.pyc
    	modified:   canri_app/__pycache__/views.cpython-311.pyc
    	modified:   canri_app/migrations/__pycache__/0001_initial.cpython-311.pyc
    	modified:   canri_app/migrations/__pycache__/0002_remove_memberlist_member_memberlist_member.cpython-311.pyc
    	modified:   canri_app/migrations/__pycache__/0003_remove_memberlist_job_title_remove_memberlist_mbti_and_more.cpython-311.pyc
    	modified:   canri_app/migrations/__pycache__/0004_rename_member_id_teammember_member.cpython-311.pyc
    	modified:   canri_app/migrations/__pycache__/0005_alter_mbti_planning_presentation_power_and_more.cpython-311.pyc
    	modified:   canri_app/migrations/__pycache__/0006_alter_project_complete_date_and_more.cpython-311.pyc
    	modified:   canri_app/migrations/__pycache__/__init__.cpython-311.pyc
    	modified:   canri_app/urls.py
    	modified:   canri_app/views.py

--- canri_app/urls.py
+++ canri_app/urls.py
@@ -1,7 +1,7 @@
 # urls.py
 from django.urls import path
 from . import views
-from .views import projectListView,Post_projectListView,project_detail_view,team_detail_view
+from .views import projectListView,Post_projectListView,project_detail_view,team_detail_view,project_detail
 from . import api
 
 app_name = 'canri_app'
@@ -47,16 +47,15 @@ urlpatterns = [
     path('progress_within_projectlist/', views.progress_within_ProjectlistView.as_view(), name='projectlist'),
     path('post_projectlist/', views.progress_within_ProjectlistView.as_view(), name='post_projectlist'),
     path('project/', projectListView, name='project'),
-    path('post_project/', views.post_ProjectlistView.as_view(), name='post_project'),
 
     #プロジェクト詳細
     path('project_detail/',views.Project_detailView.as_view(), name='project_detail'),
     path('project/<int:project_id>/', project_detail_view, name='project_detail'),
 
     # 過去プロジェクト
-    path('past_project_list/', views.Past_ProjectListView.as_view(), name='past_project_list'),
     path('past_project/', Post_projectListView, name='past_project'),
     path('past_project_view/', views.Past_ProjectView.as_view(), name='past_project_view'),
+    path('past_project/view/<int:id>/', views.project_detail, name='past_project_view'),
     path('past_project_deleting/', views.Past_ProjectDeletingView.as_view(), name='past_project_deleting_confirmation'),
     path('past_project_deleting/deleted/', views.Project_DeletedView.as_view(), name='project_deleted'),
     path('project_save/', views.Project_Save_CompleteView.as_view(), name='project_save_complete'),
