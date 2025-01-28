0156c9a7defa42174407c26d095ffa266dae336b
harumakiohara <ktq2390319@stu.o-hara.ac.jp>
2025-01-28 11:58:27 +0900

    static

--- canri/settings.py
+++ canri/settings.py
@@ -123,13 +123,13 @@ USE_I18N = True
 USE_TZ = True
 
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
 
 STATICFILES_DIRS = [
     BASE_DIR / "static",
 ]
 
-STATIC_ROOT = 'static'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
