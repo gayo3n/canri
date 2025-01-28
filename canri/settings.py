bf74e0acc13986317eb5976f51b35e546f1d58fd
harumakiohara <ktq2390319@stu.o-hara.ac.jp>
2025-01-28 10:45:04 +0900

    settings

--- canri/settings.py
+++ canri/settings.py
@@ -28,13 +28,10 @@ SECRET_KEY = 'django-insecure-vji#31=^v!by@5s%pxc&7vjg4djeprr&(4c=x)!z@-ld-no)=b
 DEBUG = True
 
 ALLOWED_HOSTS = [
-    'localhost', 
-    '127.0.0.1',
     '23.20.19.61', 
 ]
 
 CSRF_TRUSTED_ORIGINS = [
-    'http://localhost:8000',
     'http://23.20.19.61',
 ]
 
