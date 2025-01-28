b2cb2efbc9de5de0331c8bbecfdbe599f9f7fad3
harumakiohara <ktq2390319@stu.o-hara.ac.jp>
2025-01-28 12:09:30 +0900

    settings

--- canri/settings.py
+++ canri/settings.py
@@ -126,7 +126,7 @@ USE_TZ = True
 STATIC_URL = '/static/'
 
 STATICFILES_DIRS = [
-    BASE_DIR / "static",
+    os.path.join(BASE_DIR, "static"),  # プロジェクトのルートにある static フォルダ
 ]
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
