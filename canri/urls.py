569a5fbc23d753111249bbb361bbaa3ec3bbf737
harumakiohara <ktq2390319@stu.o-hara.ac.jp>
2025-01-29 11:45:26 +0900

    実験

--- canri/urls.py
+++ canri/urls.py
@@ -25,4 +25,7 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('canri_app.urls')),
     path('', include('accounts.urls')),
-] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+] 
+
+if settings.DEBUG:
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
