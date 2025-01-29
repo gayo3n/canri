38796173ea381abb57df23d52025807c3e47d985
harumakiohara <ktq2390319@stu.o-hara.ac.jp>
2025-01-29 10:41:32 +0900

    settings迷走

--- canri/settings.py
+++ canri/settings.py
@@ -123,7 +123,8 @@ USE_I18N = True
 USE_TZ = True
 
 
-STATIC_URL = '/static/'
+STATIC_URL = '/home/ec2-user/canri/static/'
+
 
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, "static"),  # プロジェクトのルートにある static フォルダ
