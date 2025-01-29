fb5cc14053b59b652ef523dd55e7b5aa0bda971e
harumakiohara <ktq2390319@stu.o-hara.ac.jp>
2025-01-29 12:22:54 +0900

    url

--- accounts/urls.py
+++ accounts/urls.py
@@ -27,4 +27,4 @@ urlpatterns = [
     # アイコン
     path('change_employee/<int:pk>/', views.account_change_employee, name='account_change_employee'),
     path('change_employee_complete/<int:pk>/', views.account_change_complete_employee, name='account_change_complete_employee'),
-] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+] 
